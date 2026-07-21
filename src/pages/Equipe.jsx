5ae3f1e52b89115e364b30827e6e7be9b470f3b7
Jackensly <isteah.jcharles1@gmail.com>
2026-07-21 16:36:16 -0400

    feat: configuration du routage React Router avec les 4 pages

---
+++ src/pages/Equipe.jsx
@@ -0,0 +1,10 @@
+function Equipe() {
+  return (
+    <section>
+      <h2>Notre Équipe</h2>
+      <p>Ici s'afficheront les membres de l'équipe.</p>
+    </section>
+  );
+}
+
+export default Equipe;
---
+++ src/pages/Projets.jsx
@@ -0,0 +1,10 @@
+function Projets() {
+  return (
+    <section>
+      <h2>Nos Projets</h2>
+      <p>Ici s'afficheront nos projets.</p>
+    </section>
+  );
+}
+
+export default Projets;
