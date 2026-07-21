2fa53b8322497748d608e1dd7d72403442cb3292
Jackensly <isteah.jcharles1@gmail.com>
2026-07-21 16:07:18 -0400

    feat: creation des composants Header, Footer et MainLayout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2026 - Portfolio Professionnel - Tous droits réservés</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>Mon Portfolio d'Équipe</h1>
+      <nav>
+        <ul>
+          <li>Accueil</li>
+          <li>Équipe</li>
+          <li>Projets</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
